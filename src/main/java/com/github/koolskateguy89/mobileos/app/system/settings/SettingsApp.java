5d87d4cb49895cee836592ec4596dfbceb8d352b
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2021-07-03 04:31:23 +0100

    :lipstick: Update SettingsApp icon

--- src/main/java/com/github/koolskateguy89/mobileos/app/system/settings/SettingsApp.java
+++ src/main/java/com/github/koolskateguy89/mobileos/app/system/settings/SettingsApp.java
@@ -45,7 +45,7 @@ public final class SettingsApp extends App {
 
 	@Getter @LombokOverride
 	// Icon made by srip (www.flaticon.com/authors/srip) from www.flaticon.com
-	private final Image icon = new Image("https://image.flaticon.com/icons/png/512/2092/2092119.png");
+	private final Image icon = new Image("https://image.flaticon.com/icons/png/512/2092/2092081.png");
 
 	@Override
 	public void onOpen() {
